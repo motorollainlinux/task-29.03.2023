a1b676d41561848fce44c7a05049c2563d961091
Кирилл <112952627+motorollainlinux@users.noreply.github.com>
2023-04-05 11:53:24 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,52 @@
 #include <iostream>
 #include <stdio.h>
+#include <cstring>
+
+using std::cout;
 
 class Person {
     public:
-    
     int ID, age, ParentID; 
-    std::string birth, job, joblog, surname, name;
-    
-    Person(int ID, std::string surname, std::string name, int age,
-    std::string birth, int ParentID, std::string job, std::string joblog) {
-        this -> ID = ID;
-        this -> surname = surname;
-        this -> name = name;
-        this -> age = age;
-        this -> birth = birth;
-        this -> ParentID = ParentID;
-        this -> job = job;
-        this -> joblog = joblog;
+    std::string birth; 
+    const char *joblog, *job, *surname, *name;
+    Person(int ID, const char *surname, const char *name, int age,
+    std::string birth, int ParentID, const char *job, const char *joblog) {
+        this->ID = ID;
+        this->surname = surname;
+        this->name = name;
+        this->age = age;
+        this->birth = birth;
+        this->ParentID = ParentID;
+        this->job = job;
+        this->joblog = name + ".txt";
     }
-    void GiveBorn(std::string newname) {
-        std::string newjoblog = (surname[0] + newname[0] + name[0] + ".txt");
-        /*test*/ std::cout << "test:" << newjoblog << "\n"; /*test*/
-        //FILE PersonJobLog = fopen(newjoblog, "w");
-        //Person* newperson = new Person(ID+1, surname, newname, 0, "29.03.2023", ID, "-", newjoblog);
-    };
+    void ChangeJob(const char *newjob) {
+        FILE *personjob;
+        personjob = fopen(joblog, "a");
+        fprintf(personjob, "%s job changed to %s job \n", job, newjob);
+        fclose(personjob);
+    }
+Person* Born(Person *newperson, const char *newname) {
+    Person *child = new Person(ParentID+1, newperson->surname, newname, 0, "05.04.2023", newperson->ID, "-", strcat((char*)newname, ".txt"));
+    return child;
 };
+};
+
+void CoutPerson(Person* newperson) {
+    cout << newperson->ID << " | " <<
+    newperson-> surname << " | "
+    << newperson->name << " | " 
+    << newperson->age << " | "
+    << newperson->birth << " | "
+    << newperson->ParentID << " | "
+    << newperson->job << " | "
+    << newperson->joblog << "\n";
+}
 int main() {
     Person* newperson = new Person(172, "Samojlov", "Kirill", 16, "19.07.2006", 0, "-", "SKD.txt");
-    newperson -> GiveBorn("Ahot");
+    Person* child = newperson.Born(newperson, "Ahot");
+    newperson->ChangeJob("barkery");
+    cout << child->joblog << "//worck?\n";
+    CoutPerson(newperson);
+    CoutPerson(child);
 }
